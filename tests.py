c0ed7800c3ac5ad924be0b20acf0cccea1181dc9
Will Maier <willmaier@ml1.net>
2010-11-19 11:04:43 -0600

    Add StatzHandler tests.

--- tests.py
+++ tests.py
@@ -7,6 +7,26 @@ class FakeRecord(object):
         for k, v in extra.items():
             setattr(self, k, v)
 
+class StatzHandlerTests(unittest.TestCase):
+
+    def cls(self):
+        from statzlogger import StatzHandler as cls
+        return cls
+    
+    def init(self, *args, **kwargs):
+        return self.cls()(*args, **kwargs)
+    
+    def record(self, *args, **kwargs):
+        return FakeRecord(*args, **kwargs)
+    
+    def test_getindices(self):
+        obj = self.init()
+        record = self.record("foo", extra=dict(
+            index="index",
+        ))
+        indices = obj.getindices(record)
+        self.assertEqual(indices, ["index"])
+
 class CollectionTests(unittest.TestCase):
 
     def cls(self):
