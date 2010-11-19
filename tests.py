816a19b786cacb8d7d1730af2688f572599de812
Will Maier <willmaier@ml1.net>
2010-11-19 10:37:47 -0600

    Test Collection, actually.

--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
 import unittest
 
-class StatzHandlerTests(unittest.TestCase):
+class CollectionTests(unittest.TestCase):
 
     def cls(self):
-        from statzlogger import StatzHandler
-        return StatzHandler
+        from statzlogger import Collection as cls
+        return cls
     
     def init(self, *args, **kwargs):
         return self.cls()(*args, **kwargs)
